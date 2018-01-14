10cb626b98b2db406aeaa6085fddda9fa0de28d9
Kaloyan Madjunov <k.madjunov@gmail.com>
2018-01-15 00:24:30 +0200

    Fix tabulation

--- spec/controllers/encrypt_messages_controller_spec.rb
+++ spec/controllers/encrypt_messages_controller_spec.rb
@@ -5,16 +5,16 @@ RSpec.describe EncryptMessagesController, type: :controller do
 		let(:encrypt_message) { FactoryGirl.create(:encrypt_message) }
 
 		it "assigns the requested message to encrypt_message" do
-		  get :show, params: { id: encrypt_message.id }, format: :json
+			get :show, params: { id: encrypt_message.id }, format: :json
 			expect(assigns(:message)).to eq(encrypt_message)
 		end
-		
+
 		it "renders a json object" do
 			expected = { 
-						  :message => "Mene me nema v celata shema"
+				:message => "Mene me nema v celata shema"
 			}.to_json
 
-		  get :show, params: { id: encrypt_message.id }, format: :json
+			get :show, params: { id: encrypt_message.id }, format: :json
 			expect(response.body).to eq(expected)
 		end
 	end
@@ -23,35 +23,34 @@ RSpec.describe EncryptMessagesController, type: :controller do
 		let(:key) { FactoryGirl.create(:key) }
 
 		context "with valid attributes" do
-		  it "creates a new encrypted message" do
-		    expect{
-		      post :create, params: { id: key.id, message: FactoryGirl.build(:encrypt_message).content }, format: :json
-		    }.to change(EncryptMessage,:count).by(1)
-		  end
+			it "creates a new encrypted message" do
+				expect {
+					post :create, params: { id: key.id, message: FactoryGirl.build(:encrypt_message).content }, format: :json
+				}.to change(EncryptMessage,:count).by(1)
+			end
 
-		  it "renders a json object" do
+			it "renders a json object" do
 				encrypt_message = FactoryGirl.create(:encrypt_message)
 
 				expected = { 
-								:id => encrypt_message.id + 1
+					:id => encrypt_message.id + 1
 				}.to_json
 
-	      post :create, params: { id: key.id, message: encrypt_message.content }, format: :json
+				post :create, params: { id: key.id, message: encrypt_message.content }, format: :json
 				expect(response.body).to eq(expected)
 			end
 		end
 		
 		context "with invalid attributes" do
-		  it "does not save the new encrypted message" do
-		    expect{
-		      post :create, params: { id: key.id }, format: :json
-		    }.not_to change(EncryptMessage,:count)
-		  end
+			it "does not save the new encrypted message" do
+				expect {
+					post :create, params: { id: key.id }, format: :json
+				}.not_to change(EncryptMessage,:count)
+			end
 		  
-		  it "renders a string object" do
+			it "renders a string object" do
 				expected = "No message"
-
-	      post :create, params: { id: key.id }, format: :json
+				post :create, params: { id: key.id }, format: :json
 				expect(response.body).to eq(expected)
 			end
 		end
